0ea2bb0264d42a6cb2bf1b3ccb0b5ed9b5b5c118
David Edmondson <threehams@gmail.com>
2012-06-27 00:43:49 -0700

    Added new dictionary files to list

--- word_list.py
+++ word_list.py
@@ -2,19 +2,24 @@ import os, re
 import binary_search
 import cProfile
 
-regex_no_lowercase = re.compile("[^a-z]")
+regexNotLowercase = re.compile("[^a-z]")
+regexNotLowercaseApostrophe = re.compile("[^a-z']")
 
-type = ['letter','number','combined']
+type = ['letter','number','letterapostrophe','combined']
 wordPath = "words"
 wordFiles = [
-    {"name":"rockyou_top1000.dic",  "handle":None, "lines":1000,    "type":2},
-    #{"name":"tv-movies-1-5000", "handle":None, "lines":0},
+    {"name":"rockyou_top1000.dic",  "handle":None, "lines":1000,    "type":3},
+    {"name":"contemporaryfiction.dic","handle":None,"lines":0,      "type":0},
+    {"name":"contemporaryfiction-special.dic","handle":None,"lines":0,"type":2},
+    {"name":"english.dic",          "handle":None, "lines":639446,  "type":0},
+    {"name":"english-special.dic",  "handle":None, "lines":639446,  "type":2},
+    {"name":"tv-1-5000.dic",        "handle":None, "lines":4995,    "type":0},
+    {"name":"tv-1-5000-special.dic","handle":None, "lines":4995,    "type":2},
     {"name":"names.dic",            "handle":None, "lines":4425,    "type":0},
-    #{"name":"names_male.dic",       "handle":None, "lines":7084,    "type":0},
-    #{"name":"names_female.dic",     "handle":None, "lines":7084,    "type":0},
     {"name":"places.dic",           "handle":None, "lines":15450,   "type":0},
     {"name":"cities.dic",           "handle":None, "lines":123011,  "type":0},
-    #{"name":"tv-movies-5000-10000", "handle":None, "lines":0},
+    {"name":"tv-5000-10000.dic",    "handle":None, "lines":4995,    "type":0},
+    {"name":"tv-5000-10000-special.dic","handle":None,"lines":4995, "type":2},
     {"name":"bible_books.dic",      "handle":None, "lines":81,      "type":0},
     {"name":"sports.dic",           "handle":None, "lines":684,     "type":0},
     {"name":"sports_proteams.dic",  "handle":None, "lines":130,     "type":0}]
@@ -30,7 +35,7 @@ def findWord(word):
 
     lineCount = 0
     for fn in wordFiles:
-        if re.search(regex_no_lowercase, word) and fn["type"] != 0:
+        if re.search(regexNotLowercase, word) and fn["type"] == 0:
             continue
         location = binary_search.searchFile(fn["handle"], word)
         if location:
